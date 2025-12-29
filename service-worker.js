2b3d3dd604db5f21800f9a3be350cb14161832b7
Saket <ITAssociate-1@natcopharma.co.in>
2025-12-29 14:38:31 +0530

    css

--- service-worker.js
+++ service-worker.js
@@ -1,36 +1,42 @@
-const CACHE_NAME = "offline-form-v1";
+const CACHE_NAME = "offline-form-v2";
 
 const FILES_TO_CACHE = [
-  "/Offline-form/",
-  "/Offline-form/index.html",
-  "/Offline-form/form.js",
-  "/Offline-form/background.jpeg",
-  "https://cdn.tailwindcss.com"
+    "/Offline-form/",
+    "/Offline-form/index.html",
+    "/Offline-form/form.js",
+    "/Offline-form/offline-db.js",
+    "/Offline-form/tailwind.css",
+    "/Offline-form/background.jpeg"
 ];
 
-// Install
+
 self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
-  self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    );
+    self.skipWaiting();
 });
 
-// Activate
+
 self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-    )
-  );
-  self.clients.claim();
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys.map(key => key !== CACHE_NAME && caches.delete(key))
+            )
+        )
+    );
+    self.clients.claim();
 });
 
-// Fetch
+
 self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response =>
-      response || fetch(event.request)
-    )
-  );
+    event.respondWith(
+        fetch(event.request).catch(() =>
+            caches.match(event.request).then(res => {
+                return res || caches.match("/Offline-form/index.html");
+            })
+        )
+    );
 });
+
