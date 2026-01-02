3ed5a12a4479f23cf3d29bb3bea457457785c7b1
Saket <ITAssociate-1@natcopharma.co.in>
2026-01-02 10:08:53 +0530

    new fixes

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,88 @@
-
-
-const CACHE_NAME = "offline-form-v4";
+const CACHE_NAME = "offline-form-v5";
 
 const FILES_TO_CACHE = [
-    "/Offline-form/",
-    "/Offline-form/index.html",
-    "/Offline-form/styles.css",
-    "/Offline-form/form.js",
-    "/Offline-form/offline-db.js",
-    "/Offline-form/BG2.jpeg",
-    "/Offline-form/BG1.jpg",
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./form.js",
+  "./offline-db.js",
+  "./BG1.jpg",
+  "./BG2.jpeg"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-    );
-    self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
-        )
-    );
-    self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
-    if (event.request.mode === "navigate") {
-        event.respondWith(
-            caches.match("/Offline-form/index.html")
-        );
-        return;
-    }
-
-    event.respondWith(
-        caches.match(event.request).then(res => res || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
+
+
+
+
+// const CACHE_NAME = "offline-form-v4";
+
+// const FILES_TO_CACHE = [
+//     "/Offline-form/",
+//     "/Offline-form/index.html",
+//     "/Offline-form/styles.css",
+//     "/Offline-form/form.js",
+//     "/Offline-form/offline-db.js",
+//     "/Offline-form/BG2.jpeg",
+//     "/Offline-form/BG1.jpg",
+// ];
+
+// self.addEventListener("install", event => {
+//     event.waitUntil(
+//         caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+//     );
+//     self.skipWaiting();
+// });
+
+// self.addEventListener("activate", event => {
+//     event.waitUntil(
+//         caches.keys().then(keys =>
+//             Promise.all(keys.map(key => key !== CACHE_NAME && caches.delete(key)))
+//         )
+//     );
+//     self.clients.claim();
+// });
+
+// self.addEventListener("fetch", event => {
+//     if (event.request.mode === "navigate") {
+//         event.respondWith(
+//             caches.match("/Offline-form/index.html")
+//         );
+//         return;
+//     }
+
+//     event.respondWith(
+//         caches.match(event.request).then(res => res || fetch(event.request))
+//     );
+// });
+
+
+
